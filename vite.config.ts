0a1be321265f989eefdee061e16026c059b7fbb0
Parag Goyal <1997parag.goyal@gmail.com>
2025-11-30 14:52:44 +0530

    add the gh-pages dev dependency and predeploy, deploy script to deploy the app to github page

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://paraggoyal28.github.io/pathfinding-visualizer/',
   plugins: [
     react(),
   ],
